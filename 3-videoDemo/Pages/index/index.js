1016689ae37c195f59b8b08c1c29698fb41f29ba
huangli <huanglixianhui@163.com>
2020-10-23 22:00:03 +0800

    实现弹幕和切换视频功能

--- 3-videoDemo/Pages/index/index.js
+++ 3-videoDemo/Pages/index/index.js
@@ -33,7 +33,7 @@ Page({
       {
         id:'4',
         title:'forthVideo',
-        videoUrl:"http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
+        videoUrl:"http://www.w3school.com.cn//i/movie.mp4"
         
       },
     ],
